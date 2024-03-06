b293300f66c65e053a682facfd35a757e6392523
Ailin Lamas <lamasailin@gmail.com>
2024-03-05 21:36:56 -0300

    reseteo de pc

---
+++ U1M4/js.js
@@ -0,0 +1,72 @@
+/*Si quiero ver que una condición se cumple uso while. No es necesario utilizarlo con listas. Si la condición se cumple (true) ejecuta lo que tiene entre llaves, cuando no se cumple la condición dada (false) deja de ejecutarse. Se escribe 
+    
+    while(condición){
+           }
+           let valor=prompt(“ingresar dato”,”ingrese dato para finalizar”)
+Ej:
+        while(valor.toLowerCase() !==”fin”){
+        console.log(“hola!+valor)
+        valor=prompt(“Ingrese otro nombre. Para finalizar, coloque fin”)
+
+        /*Utilizando lo aprendido escribir el código
+necesario para que dada una distancia determine
+el medio de transporte apropiado de acuerdo a 
+las siguientes reglas:
+
+0 a 1000 metros = pie
+1000 a 10000 metros = bicicleta
+10000 a 30000 metros = colectivo
+30000 a 100000 metros = auto
++100000 = avión*/
+
+prompt("¡Hola! Te esperamos en nuestro lugar. Por favor, ingrese los metros de distancia para poder indicarle el mejor medio de transporte para llegar");
+
+
+/*
+ pie <=1000
+
+ bicicleta <1000 & >=10000
+
+ colectivo <10000 & >=30000
+
+auto <30000 & >=1000000
+
+ avión >1000000
+*/
+
+const valr=parseInt(prompt('Visita mi oficina. Ingresa tu distancia y te recomiendo un medio de transporte.', ''));
+  
+switch (valor) {
+  case Pie <=1000:
+    document.write('Pie');
+    break;      
+  case Bicicleta  <1000 & >=10000:
+    document.write('Bicicleta');
+    break;
+  case Colectivo <10000 & >=30000:
+    document.write('Colectivo');
+    break;
+  case Auto <30000 & >=1000000:
+    document.write('cuatro');
+    break;
+  case 5:
+    document.write('cinco');
+    break;
+  default:
+    document.write('debe ingresar un valor comprendido entre 1 y 5.');
+}
+
+let distancia=parseInt(prompt("Visita mi oficina. Ingresa tu distancia y te recomiendo un medio de transporte."))
+
+
+
+
+
+
+while(pie=true){
+}
+let valor=prompt(“ingresar dato”,”ingrese dato para finalizar”)
+Ej:
+while(valor.toLowerCase() !==”fin”){
+console.log(“hola!+valor)
+valor=prompt(“Ingrese otro nombre. Para finalizar, coloque fin”)
